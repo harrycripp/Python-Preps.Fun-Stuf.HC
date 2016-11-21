4376e873bcafcc4ed75439d29f6c8624d744a5fa
Harry Cripp <criphe2015@cranleigh.org>
2016-11-21 09:45:03 +0000

    Add files via upload

---
+++ Counting Prep.py
@@ -0,0 +1,7 @@
+firstnumber = input("Please enter the first number: ")
+secondnumber = input("Please enter second number: ")
+finalnumber = input("Please enter final number: ")
+
+firstnumber + secondnumber + finalnumber
+print("The computer has just added up all the numbers that you have just enetred, and the answer is: " answer)
+
